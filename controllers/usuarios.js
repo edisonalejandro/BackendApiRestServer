3d40b31eb99524fbb373241085fce27d81a796ba
EdisonAlejandro <arriagada.ed@gmail.com>
2021-10-02 14:59:59 -0300

    Segundo Commit

--- controllers/usuarios.js
+++ controllers/usuarios.js
@@ -1,6 +1,8 @@
-const {
-    response
-} = require('express');
+const { response } = require('express');
+const bcryptjs = require('bcryptjs');
+
+const Usuario = require('../models/usuario');
+
 
 const usuariosGet = (req = request, res = response) => {
 
@@ -12,14 +14,24 @@ const usuariosGet = (req = request, res = response) => {
 };
 
 
-const usuariosPost = (req, res = response) => {
+const usuariosPost = async(req, res = response) => {
 
-    const {nombre, edad} = req.body;
+    const { nombre, correo, password, rol} = req.body;
+    const usuario = new Usuario( { nombre, correo, password, rol} );
+
+    // Verificar si el correo existe
+
+    // Encriptar la contraseña
+    const salt = bcryptjs.genSaltSync();
+    usuario.password = bcryptjs.hashSync( password, salt );
+    
+    // Guardar en base de datos
+
+    await usuario.save();
 
     res.json({
         msg: 'post API - controlador',
-        nombre,
-        edad
+        usuario
     });
 };
 
